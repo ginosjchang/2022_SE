33d26c66561e6a41c9372de062df455cec235b21
ginosjchang <ginosjchang@gmail.com>
2022-03-29 22:02:22 +0800

    Upload image

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    
+
     app.run(port=7414)
--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './upload'
 
 def create_app():
     app = Flask(__name__)
+    global UPLOAD_FOLDER
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
     app.add_url_rule('/','login', login, methods=['GET', 'POST'])
     app.add_url_rule('/home/<username>','home', home)
-    app.add_url_rule('/function/<username>', 'function', function)
+    app.add_url_rule('/upload/<username>', 'upload', function, methods=['GET', 'POST'])
     app.add_url_rule('/search/<username>', 'search', search)
     return app
 
@@ -16,7 +22,13 @@ def login():
 def home(username):
     return render_template('home.html', name=username)
 
-def function(username):
+def upload(username):
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            global UPLOAD_FOLDER
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
     return render_template('function.html', name=username)
 
 def search(username):
